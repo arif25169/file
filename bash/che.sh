2d8c295775e793367c379a8f525dd316e7555a73
Arif Hasan <arif25169@gmail.com>
2019-03-21 00:58:56 +0600

    Update che.sh

--- bash/che.sh
+++ bash/che.sh
@@ -2,20 +2,56 @@
 function program_doesnt_exist {
   local return_=1
   type $1 >/dev/null 2>&1 || { local return_=0; }
+  
   # return $return_ instead of printing
-  return $return_
+if [ "$return_" -eq "1" ]; then
+   echo "but it is already installed";
+fi
+ return $return_
+
 }
 
-emails=('test@abc.com' 'test@xyz.org')
-passwords=('admin123' 'passwd1!')
-for (( i = 0; i < ${#emails[@]}; ++i )); do
-    echo "Email: ${emails[i]}"
-    echo "Password: ${passwords[i]}"
-done
+# emails=('testabc.com' 'testxyz.org')
+# passwords=('admin123' 'passwd1!')
+# for (( i = 0; i < ${#emails[@]}; ++i )); do
+#     echo "Email: ${emails[i]}"
+#     echo "Password: ${passwords[i]}"
+# done
+# if grep -q "dicounter_${string1}_from_${string2}" MasterFile.txt; then 
+#    echo "dicounter_${string1}_from_${string2} already exists in the MasterFile. Would you like to proceed?"
+#    read string3
+#    if [[ "${string3^}" == 'Y' ]]; then
+#       screen -S trans -L /dev/ttyACM0
+#       screen -S trans -X stuff 's'$(echo -ne '\015')
+#       sleep 8s
+#       screen -S trans -X quit
+#    else
+#        exit 0
+#    fi
+# else
+#     #opening screen & begin analysis
+#     screen -S trans -L /dev/ttyACM0
+#     screen -S trans -X stuff 's'$(echo -ne '\015')
+#     sleep 8s
+#     screen -S trans -X quit
+# fi
+Str=$(ldconfig -p | grep libghfh)
+echo ${#Str}
+if [[ ${#Str} == '0' ]]; then
+#sudo apt install 4pane
+  wget -q -O /tmp/libpng12.deb http://mirrors.kernel.org/ubuntu/pool/main/libp/libpng/libpng12-0_1.2.54-1ubuntu1_amd64.deb
+  sudo dpkg -i /tmp/libpng12.deb
+  rm /tmp/libpng12.deb
+else
+  exit 0
+fi
 
-# install nodejs
-if program_doesnt_exist node; then
-echo ""
-  curl -sL https://deb.nodesource.com/setup_11.x | sudo -E bash -
-  sudo apt install -y nodejs
+apps=('node' 'node')
+echo "Installing: ${apps[0]}"
+if program_doesnt_exist ${apps[0]}; then
+  sudo apt install -y ${apps[0]}
+fi
+echo "Installing: ${apps[1]}"
+if program_doesnt_exist ${apps[1]}; then
+  sudo apt install -y ${apps[1]}
 fi
